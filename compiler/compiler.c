030f28bc224c6b478411c2054dc99fb62ea46944
qertend <szucslevente607@gmail.com>
2024-10-02 21:55:30 +0200

    pretty c:

--- compiler/compiler.c
+++ compiler/compiler.c
@@ -9,17 +9,16 @@ int main(int argc, char const *argv[])
     //check if a filename was provided
     if (argc < 2) {
         //if not, print error message and exit
-        printf(textColorRed "ERROR:" textColorDefault " No file specified");
+        printf(textColorRed "ERROR:" textColorDefault " No file specified\n");
         return 1;
     }
     rawInput = fopen(argv[1], "r");
     //check if filename is correct
     if (rawInput == NULL) {
         //if not, print error message and exit
-        printf(textColorRed "ERROR:" textColorDefault  "No such file exists");
+        printf(textColorRed "ERROR:" textColorDefault  "No such file exists\n");
         return 1;
     }
-
 
     //process file here
 
@@ -27,13 +26,13 @@ int main(int argc, char const *argv[])
 
     //prepare bf instructions
 
-    FILE* out = fopen("out.exe", "w");
+    FILE* out = fopen("out.txt", "w");
 
     //output bf file here
 
     fclose(out);
 
-    printf("%s", argv[1]);
+    printf("%s\n", argv[1]);
     return 0;
 }
 
